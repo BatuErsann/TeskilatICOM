14abda07a4fd5b29e9d3c0bf532df1206abd31c7
Batuhan Ersan <batuhan.ersan81@gmail.com>
2026-02-10 00:24:17 +0300

    tiny fix

--- teskilat/frontend/src/components/Navbar.jsx
+++ teskilat/frontend/src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+  import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { FaInstagram, FaLinkedin, FaYoutube, FaVimeoV, FaBars, FaTimes } from 'react-icons/fa';
 
